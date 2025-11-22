c95e8381075005179922e3eb422c19f06e9dd5b0
Nikita Lutik <netsket10@yandex.by>
2025-11-22 21:10:04 +0300

    Tasks from day 2 (completed)

---
+++ Day 2/Number Manipulation.py
@@ -0,0 +1,20 @@
+# bmi = 84 / 1.65 ** 2
+# print(round(bmi,2))
+
+# +=
+# -=
+# *=
+# /=
+
+score = 0
+
+score += 1 # plus equals means +1 to the previous parameter
+print(score)
+
+# f-string
+
+print("Your score is " + str(score))
+
+# print(f"")
+height = input("Enter your height: ")
+print(f"Your score is {score}, your height is {height}!")
