d99f81e31694de49f63e657146a5dbb1d184a8a1
Nikita Lutik <netsket10@yandex.by>
2025-11-22 21:19:02 +0300

    Tasks from day 1 (completed)

---
+++ Day 1/Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Hello! Here is a program, which may help you to come up with the name of your band!")
+city = input("What city did you grow up in?\n")
+petname = input("What is your pet name?\n")
+print("Your preferred band name is " + city + " " + petname)
---
+++ Day 1/Variables.py
@@ -0,0 +1,5 @@
+print("The number of your name's characters is " + str(len(input("What is your name? "))))
+
+username = input("What is your name? ")
+length = len(username)
+print(length)
