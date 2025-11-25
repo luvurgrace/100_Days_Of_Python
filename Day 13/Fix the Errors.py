c7e875324c90615422577219ae6fc12b8586bd64
Nikita Lutik <netsket10@yandex.by>
2025-11-25 13:29:24 +0300

    Tasks from day 13 (completed)

---
+++ Day 13/Fix the Errors.py
@@ -0,0 +1,8 @@
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("You typed invalid number. Try again with a numerical number. ")
+    age = int(input("How old are you?"))
+
+if age > 18:
+    print(f"You can drive at age {age}.")
