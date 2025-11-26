7f3d4d1eda936376744e8d2eab92a1dd0ddd0cc4
Nikita Lutik <netsket10@yandex.by>
2025-11-26 12:57:58 +0300

    Tasks from day 18 (completed)

---
+++ Day 18/turtle spawn.py
@@ -0,0 +1,8 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle() # spawns a turtle
+timmy_the_turtle.shape("turtle") # change our turtle's shape
+timmy_the_turtle.color("blue")
+
+screen = Screen()
+screen.exitonclick() # display that may be exited by clicking
