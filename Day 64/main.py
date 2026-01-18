3562b5c60b9739bc34934014372838709e01d177
Nikita Lutik <netsket10@yandex.by>
2026-01-18 16:33:38 +0300

    Tasks from day 64 (completed)

--- Day 64/main.py
+++ Day 64/main.py
@@ -42,17 +42,21 @@ class Movie(db.Model):
     review: Mapped[str] = mapped_column(String(250), nullable=True)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
+
 class MovieForm(FlaskForm):
     movie_rating = StringField('Movie rating out of 10', validators=[DataRequired()])
     movie_review = StringField('Review', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
 class AddMovieForm(FlaskForm):
     title = StringField('Movie Title', validators=[DataRequired()])
     submit = SubmitField('Add Movie')
 
+
 with app.app_context():
     db.create_all()
+
 
 # new_movie = Movie(
 #     title="Phone Booth",
@@ -74,17 +78,19 @@ def home():
     movies = db.session.execute(db.select(Movie)).scalars().all()
     return render_template("index.html", movies=movies)
 
+
 @app.route("/edit", methods=["GET", "POST"])
 def rate_movie():
-    form = MovieForm() # create a form
-    movie_id = request.args.get("id") # return film's id
-    movie = db.get_or_404(Movie, movie_id) # search for film in database
-    if form.validate_on_submit(): # if form is submitted
-        movie.rating = float(form.movie_rating.data) # rating update
-        movie.review = form.movie_review.data # review update
-        db.session.commit() # confirm changes
+    form = MovieForm()  # create a form
+    movie_id = request.args.get("id")  # return film's id
+    movie = db.get_or_404(Movie, movie_id)  # search for film in database
+    if form.validate_on_submit():  # if form is submitted
+        movie.rating = float(form.movie_rating.data)  # rating update
+        movie.review = form.movie_review.data  # review update
+        db.session.commit()  # confirm changes
         return redirect(url_for('home'))
     return render_template("edit.html", movie=movie, form=form)
+
 
 @app.route("/delete")
 def delete_movie():
@@ -95,40 +101,40 @@ def delete_movie():
     print(movie_to_delete)
     return redirect(url_for('home'))
 
-@app.route("/add", methods = ["GET", "POST"])
+
+@app.route("/add", methods=["GET", "POST"])
 def add_movie():
     add_form = AddMovieForm()
     if add_form.validate_on_submit():
         movie_title = add_form.title.data
         response = requests.get(MOVIE_DB_SEARCH_URL, params={
             "api_key": MOVIE_DB_API_KEY, "query": movie_title}
-        )
+                                )
         data = response.json()["results"]
         return render_template("select.html", options=data)
     return render_template("add.html", form=add_form)
+
 
 @app.route("/find")
 def find_movie():
     movie_api_id = request.args.get("id")
     if movie_api_id:
-        movie_api_url = f"{MOVIE_DB_SEARCH_URL}/{movie_api_id}"
-        response = requests.get(movie_api_url, params={
-            "api_key": MOVIE_DB_API_KEY,
-            "language": "en-US"
-        })
-
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
         data = response.json()
-        print(data)
+
+        # print(f"poster_path: {data.get('poster_path')}")
+        # print(f"Full URL: {MOVIE_DB_IMAGE_URL}{data.get('poster_path')}")
 
         new_movie = Movie(
-            title = data["title"],
-            year = data["release_date"].split("-")[0],
-            img_url = f"{MOVIE_DB_SEARCH_URL}{data["poster_path"]}",
-            description = data["overview"]
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
         )
         db.session.add(new_movie)
         db.session.commit()
-        return redirect(url_for("home"))
+        return redirect(url_for("rate_movie", id=new_movie.id))
 
 
 if __name__ == '__main__':
